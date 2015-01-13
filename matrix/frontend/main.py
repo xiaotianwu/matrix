228a4e00d6086c6ab2eb39a1125988dd1d8ea8a7
xiaotian.wu <xiaotian.wu@chinacache.com>
2015-01-13 06:45:02 +0000

    add flask test

---
+++ matrix/frontend/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+if __name__ == '__main__':
+  app.run(host = '223.202.46.132', port = 20000)
+  #app.run(host = '127.0.0.1')
