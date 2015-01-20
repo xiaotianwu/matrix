6b716904e2a4949036b2e0041e064ac2994c7079
xiaotian.wu <xiaotian.wu@chincache.com>
2015-01-20 09:05:29 +0000

    add task template

--- matrix/service/service.py
+++ matrix/service/service.py
@@ -5,44 +5,43 @@ __author__ = 'xiaotian.wu@chinacache.com'
 import socket
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 
+from kazoo.client import KazooClient
+
 from matrix.core.config import config
 from matrix.core.framework import MatrixFramework
 from matrix.core.logger import logger
 from matrix.core.task import Task, TaskProperty
 
-framework = MatrixFramework("MatrixTest", "1", "223.202.46.153:2181")
-framework.install()
+zk_client = KazooClient(hosts = "223.202.46.153:2181")
+zk_client.start()
+framework = MatrixFramework("MatrixTest", "1", zk_client)
 framework.start()
 
 def hello():
   return "hello"
 
-def add(task_id, task_name, docker_image, command, cpus, mem, host):
+def add(task_name, docker_image, command, cpus, mem, host):
   task = Task()
   task.property.append(TaskProperty.AutoRecover)
-  task.id = task_id
+  if len(task_name) == 0:
+    return False
+  if cpus <= 0 or mem <= 0:
+    return False
   task.docker_image = docker_image
   task.command = command
   task.name = task_name
   task.constraint.cpus = cpus
   task.constraint.mem = mem
   task.constraint.host = host
-  print task.id
-  print task.docker_image
-  print task.command
-  print task.name
-  print task.command
-  print task.constraint.cpus
-  print task.constraint.mem
-  print task.constraint.host
   framework.add(task)
   return True
 
 def delete(task_id):
+  framework.delete(task_id)
   return True
 
 def get(task_id):
-  return True
+  return framework.get(task_id)
 
 def list_all():
   return True
@@ -50,7 +49,7 @@ def list_all():
 if __name__ == '__main__':
   ip = socket.gethostbyname(socket.gethostname())
   port = 30000
-  logger.info('start server at host %s:%s' %(ip, port))
+  logger.info('start matrix web service at host %s:%s' %(ip, port))
 
   server = SimpleXMLRPCServer((ip, port))
   server.register_introspection_functions()
--- matrix/test/add_task.py
+++ matrix/test/add_task.py
@@ -4,6 +4,6 @@ from xmlrpclib import ServerProxy
 
 matrix = ServerProxy("http://223.202.46.132:30000")
 
-#matrix.add(1, "WalleMonitor", "cpdc/walle_monitor", "/walle_monitor_0_2_1/monitor/main.py --kafka_hosts=223.203.199.153:9092,223.203.199.152:9092,223.203.199.150:9092,223.203.199.149:9092,223.203.199.148:9092 --topic=final-log --ip_mapping_file=/walle_monitor_0_2_1/monitor/ipmapping.conf", 1, 2048, "MIS-BJ-6-5A2")
+matrix.add("WalleMonitor", "cpdc/walle_monitor", "/walle_monitor_0_2_1/monitor/main.py --kafka_hosts=223.203.199.153:9092,223.203.199.152:9092,223.203.199.150:9092,223.203.199.149:9092,223.203.199.148:9092 --topic=final-log --ip_mapping_file=/walle_monitor_0_2_1/monitor/ipmapping.conf", 1, 2048, "MIS-BJ-6-5A2")
 
-matrix.add(2, "StormNimbus", "cpdc/storm_nimbus", "su storm&&nohup /usr/lib/apache-storm-0.9.3/bin/storm nimbus", 2, 2048, "MIS-BJ-6-5A1")
+#matrix.add(2, "StormNimbus", "cpdc/storm_nimbus", "su storm&&nohup /usr/lib/apache-storm-0.9.3/bin/storm nimbus", 2, 2048, "MIS-BJ-6-5A1")
