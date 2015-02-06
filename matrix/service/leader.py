0c33b48dc11c6bf997d1ed8af756d2eeb254eaea
xiaotian.wu <xiaotian.wu@chincache.com>
2015-02-06 07:28:01 +0000

    add leader

--- matrix/service/leader.py
+++ matrix/service/leader.py
@@ -3,19 +3,26 @@ __author__ = 'xiaotian.wu@chinacache.com'
 import socket
 
 from flask import Flask
-from flask import request
+from flask.ext.restful import reqparse, abort, Api, Resource
 from kazoo.client import KazooClient
 
 from matrix.core.config import config
 from matrix.core.framework import MatrixFramework
 from matrix.core.logger import logger
-from matrix.service.api import add, delete, get
+from matrix.service.api import add, delete, get, health
 from matrix.service.flags import parse_flag
 
 flags = parse_flag()
 ip = socket.gethostbyname(socket.gethostname())
 app = Flask(__name__)
-port = flags.rest_port
+api = Api(app)
+parser = reqparse.RequestParser()
+parser.add_argument('name', type = str)
+parser.add_argument('image', type = str)
+parser.add_argument('command', type = str)
+parser.add_argument('cpus', type = int)
+parser.add_argument('mem', type = int)
+parser.add_argument('host', type = str)
 
 zk_client = KazooClient(hosts = flags.zk)
 zk_client.start()
@@ -24,26 +31,59 @@ framework_name = flags.framework_name
 framework_id = flags.framework_id
 framework = MatrixFramework(flags.mesos, framework_name, framework_id, zk_client)
 
-@app.route('/add', methods = ['POST'])
-def add_task():
-  return add()
+class Status(Resource):
+  def get(self):
+    return health()
 
-@app.route('/delete', methods = ['POST'])
-def delete_task():
-  return delete()
+api.add_resource(Status, '/matrix/status')
 
-@app.route('/get', methods = ['POST'])
-def get_task():
-  return get()
+class Get(Resource):
+  def get(self, task_id):
+    if task_id == "*":
+      return "get all task", 200
+    else:
+      get(framework, task_id)
+      return "get task " + str(task_id), 200
 
-def run():
-  election = zk_client.Election(framework_name + "/" + framework_id, ip)
-  election.run(i_am_leader)
+api.add_resource(Get, '/matrix/get/<task_id>')
+
+class Create(Resource):
+  def post(self):
+    args = parser.parse_args()
+    if args['name'] is None or\
+       args['image'] is None or\
+       args['command'] is None or\
+       args['cpus'] is None or\
+       args['mem'] is None or\
+       args['host'] is None:
+      return "some requirement field not filled", 400
+    else:
+      add(framework,
+          args['name'],
+          args['image'],
+          args['command'],
+          args['cpus'],
+          args['mem'],
+          args['host'])
+      return "task created", 200
+
+api.add_resource(Create, '/matrix/create')
+
+class Delete(Resource):
+  def post(self, task_id):
+    delete(framework, task_id)
+    return "task deleted", 200
+
+api.add_resource(Delete, '/matrix/delete/<task_id>')
 
 def i_am_leader():
   logger.info("elected as leader, start leader mode")
   framework.start()
-  app.run(host = ip, port = port)
+  app.run(host = ip, port = flags.rest_port)
+
+def run():
+  election = zk_client.Election(framework_name + "/" + framework_id, ip)
+  election.run(i_am_leader)
 
 if __name__ == '__main__':
   run()
